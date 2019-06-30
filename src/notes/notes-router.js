872aca96abacb104d25d4b2e890f0b2bbbc0d90c
Robert Wiggins <wigginsro11@gmail.com>
2019-06-30 11:23:38 -0400

    added functional integrated notes router and service

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -28,17 +28,15 @@ notesRouter
   .post(jsonBodyParser, (req, res, next) => {
     const { title, content, folderid } = req.body;
     const newNote = { title, content };
-    for (const [key, value] of Object.entries(newNote)) {
-      if (key === 'content' && !value) {
-        newNote.content = '';
-      }
-      if (key === 'title' && title === null) {
-        return res.status(400).json({
-          error: { message: `Missing ${key} in required body` },
-        });
-      }
+    if (!content) {
+      newNote.content = '';
     }
-    newNote['folderid'] = Number(folderid);
+    if (!title) {
+      return res.status(400).json({
+        error: { message: 'Missing title in required body' }, // TODO may need next to pick up error message
+      });
+    }
+    newNote['folderid'] = parseInt(folderid);
     NotesService.insertNote(req.app.get('db'), newNote)
       .then(note => {
         res
@@ -48,7 +46,33 @@ notesRouter
       })
       .catch(next);
   })
-  .patch((req, res, next) => {})
-  .delete((req, res, next) => {});
+  .patch(jsonBodyParser, (req, res, next) => {
+    const { title, content, noteId, folderId } = req.body;
+    // body requires noteId
+    const noteToUpdate = { title, content };
+
+    const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: 'Request body must contain either "title" or "content"',
+        },
+      });
+
+    const today = new Date();
+    noteToUpdate.modified = today;
+
+    NotesService.updateNote(req.app.get('db'), parseInt(noteId), noteToUpdate)
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .delete(jsonBodyParser, (req, res, next) => {
+    let noteId = req.body.noteId;
+    NotesService.deleteNote(req.app.get('db'), parseInt(noteId))
+      .then(() => res.status(204).end())
+      .catch(next);
+  });
 
 module.exports = notesRouter;

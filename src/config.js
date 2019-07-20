2ac0725c271dc1b800422c340934498fcb9e472c
Robert Wiggins <wigginsro11@gmail.com>
2019-07-20 16:07:52 -0400

    added config test db

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://noteful-guy:password@localhost/noteful',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://noteful-guy:password@localhost/noteful_test'
 };
